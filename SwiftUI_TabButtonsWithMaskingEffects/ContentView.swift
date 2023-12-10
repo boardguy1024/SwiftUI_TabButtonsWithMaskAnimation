4515ffafda265b31a1ff22b57754b0accdb05af1
paku <boardguy1024@gmail.com>
2023-12-10 23:20:46 +0900

    Initial Commit

---
+++ SwiftUI_TabButtonsWithMaskingEffects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_TabButtonsWithMaskingEffects
+//
+//  Created by paku on 2023/12/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_TabButtonsWithMaskingEffects/SwiftUI_TabButtonsWithMaskingEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TabButtonsWithMaskingEffectsApp.swift
+//  SwiftUI_TabButtonsWithMaskingEffects
+//
+//  Created by paku on 2023/12/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TabButtonsWithMaskingEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
