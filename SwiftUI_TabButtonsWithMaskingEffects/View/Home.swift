13d87dda148bcdabf22fdc13af017e7533b9bfe1
paku <boardguy1024@gmail.com>
2023-12-12 21:39:04 +0900

    maskでcurrentTabを表現

--- SwiftUI_TabButtonsWithMaskingEffects/View/Home.swift
+++ SwiftUI_TabButtonsWithMaskingEffects/View/Home.swift
@@ -11,6 +11,8 @@ struct Home: View {
     
     @State var offset: CGFloat = 0
     @State var currentTab: Tab = tabs.first!
+    @State var isTapped: Bool = false
+    
     var body: some View {
         
         GeometryReader { proxy in
@@ -36,9 +38,13 @@ struct Home: View {
                             // index0 : 0~393 * 0
                             // index1 : 0~393 * 1
                             // index2 : 0~393 * 2
-                            if currentTab == tab {
+                            if currentTab == tab && !isTapped {
                                 let offset = screenSize.width * CGFloat(indexOf(tab: tab))
                                 self.offset = value - offset
+                            }
+                            
+                            if value == 0 && isTapped {
+                                isTapped = false
                             }
                         })
                     }
@@ -62,20 +68,67 @@ struct Home: View {
                 .font(.title.bold())
                 .foregroundStyle(.white)
             
+            // MEMO:  UnderLineBarを使いたい場合は、これを使う
+//            HStack(spacing: 0) {
+//                ForEach(tabs) { tab in
+//                    Text(tab.name)
+//                        .fontWeight(.semibold)
+//                        .foregroundStyle(.white)
+//                        .frame(maxWidth: .infinity)
+//                }
+//            }
+//            .background(alignment: .bottomLeading) {
+//                Capsule()
+//                    .fill(.white)
+//                    .frame(width: size.width / CGFloat(tabs.count) - 10, height: 4)
+//                    .offset(y: 14)
+//                    .offset(x: tabOffset(size: size))
+//            }
+            
+            
             HStack(spacing: 0) {
                 ForEach(tabs) { tab in
                     Text(tab.name)
                         .fontWeight(.semibold)
-                        .foregroundStyle(.white)
+                        .padding(.vertical, 6)
+                        .foregroundColor(.white)
                         .frame(maxWidth: .infinity)
                 }
             }
-            .background(alignment: .bottomLeading) {
+            .overlay(alignment: .leading) {
                 Capsule()
                     .fill(.white)
-                    .frame(width: size.width / CGFloat(tabs.count), height: 4)
-                    .offset(y: 14)
+                     // 30: padding
+                    .frame(width: (size.width - 30) / CGFloat(tabs.count))
                     .offset(x: tabOffset(size: size))
+            }
+            .overlay(alignment: .leading) {
+                // 1. overlayで tabNameの上に同じ位置で black.tabNameを重ねる
+                HStack(spacing: 0) {
+                    ForEach(tabs) { tab in
+                        Text(tab.name)
+                            .fontWeight(.semibold)
+                            .padding(.vertical, 6)
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity)
+                            .contentShape(Capsule())
+                            .onTapGesture {
+                                isTapped = true
+
+                                withAnimation(.easeInOut) {
+                                    currentTab = tab
+                                    offset = -(size.width) * CGFloat(indexOf(tab: tab))
+                                }
+                            }
+                    }
+                }
+                .frame(width: size.width - 30)
+                .mask(alignment: .leading) {
+                    // Maskをかけて capsule内だけ black.tabNameを表示させる
+                    Capsule()
+                        .frame(width: (size.width - 30) / CGFloat(tabs.count))
+                        .offset(x: tabOffset(size: size))
+                }
             }
         }
         .frame(maxWidth: .infinity, alignment: .leading)
@@ -96,10 +149,10 @@ struct Home: View {
         // center: 0.333
         // last: 1
         
-        // 12: padding
+        // 10: padding
         
         let tabsCount = CGFloat(tabs.count)
-        let padding = tabsCount * 15
+        let padding = tabsCount * 10
         
         let underlineWidth = (size.width - padding) / tabsCount
         
